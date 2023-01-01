9c62ae4ee72b7f8da08485708efc0d927857ea7c
Daniel Fonseca <daniel_mf@live.com>
2023-01-01 19:39:39 -0300

    Getter e Setter de Video

--- src/main/java/br/com/challenge/videoflix/entity/Video.java
+++ src/main/java/br/com/challenge/videoflix/entity/Video.java
@@ -27,4 +27,31 @@ public class Video {
         this.url = url;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
