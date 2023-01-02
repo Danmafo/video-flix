b53889149fbe3c980afe2586c10b1a121b862b5d
Daniel Fonseca <daniel_mf@live.com>
2023-01-02 08:13:15 -0300

    Adicionando Validação

--- src/main/java/br/com/challenge/videoflix/dto/request/VideoRequestDto.java
+++ src/main/java/br/com/challenge/videoflix/dto/request/VideoRequestDto.java
@@ -1,8 +1,20 @@
 package br.com.challenge.videoflix.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record VideoRequestDto(
+
+        @NotBlank(message = "O título do vídeo não pode ser nulo ou vazio!")
+        @Size(max = 50, message = "O título do vídeo não pode ultrapassar {max} caracteres")
         String titulo,
+
+        @NotBlank(message = "A descrição do vídeo não pode ser nulo ou vazio!")
+        @Size(max = 150, message = "A descrição do vídeo não pode ultrapassar {max} caracteres")
         String descricao,
+
+        @NotBlank(message = "A URL do vídeo não pode ser nulo ou vazio!")
+        @Size(max = 200, message = "A URL do vídeo não pode ultrapassar {max} caracteres")
         String url
 ) {
 }
