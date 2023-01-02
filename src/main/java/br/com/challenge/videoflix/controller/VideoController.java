bb943dadf9fa3de783edc80a5538214db283fe1f
Daniel Fonseca <daniel_mf@live.com>
2023-01-02 07:53:46 -0300

    Implementado Busca por Id

--- src/main/java/br/com/challenge/videoflix/controller/VideoController.java
+++ src/main/java/br/com/challenge/videoflix/controller/VideoController.java
@@ -5,6 +5,7 @@ import br.com.challenge.videoflix.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class VideoController {
     @GetMapping
     public ResponseEntity<List<VideoResponseDto>> listar() {
         return ResponseEntity.ok(service.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VideoResponseDto> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
     }
 
 }
--- src/main/java/br/com/challenge/videoflix/service/VideoService.java
+++ src/main/java/br/com/challenge/videoflix/service/VideoService.java
@@ -3,6 +3,7 @@ package br.com.challenge.videoflix.service;
 import br.com.challenge.videoflix.dto.response.VideoResponseDto;
 import br.com.challenge.videoflix.entity.Video;
 import br.com.challenge.videoflix.repository.VideoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,11 @@ public class VideoService {
         List<Video> listaEntidade = repository.findAll();
         List<VideoResponseDto> listaDto = listaEntidade.stream().map(VideoResponseDto::converteEntidadeParaDto).toList();
         return listaDto;
+    }
+
+    public VideoResponseDto buscarPorId(Long id) {
+        Video entidade = repository.getReferenceById(id);
+        return VideoResponseDto.converteEntidadeParaDto(entidade);
     }
 
 }
