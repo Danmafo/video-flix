65117d8a3f53fdbddc9eaa5c52daa8c29ae9428a
Daniel Fonseca <daniel_mf@live.com>
2023-01-06 08:26:33 -0300

    Reformulando Dados no Banco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,10 @@
-INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Aventura', '#0000FF');
-INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Terror', '#00FF00');
-INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Romance', '#FF0000');
+INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Estudo', '#0000FF');
+INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Jogos', '#00FF00');
+INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Viagem', '#FF0000');
 
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O Senhor do Aneis: A Sociedade do Anel', 'Descricao O Senhor do Aneis: A Sociedade do Anel', 'url1', 1);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Hereditario', 'Descricao Hereditario', 'url2', 2);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Passageiros', 'Descricao Passageiros', 'url3', 3);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Spring Boot | Curso Completo 2022', 'Aula sobre o Framework Spring Boot', 'https://www.youtube.com/watch?v=LXRU-Z36GEU', 1);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O que e Angular e AngularJS?', 'Aula sobre o Framework Angular', 'https://www.youtube.com/watch?v=LFlNU30u7d8', 1);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Os Jogos Mais Esperados de 2023', 'Lista de Jogos Aguardados em 2023', 'https://www.youtube.com/watch?v=S3ElEo41sCU', 2);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Stray - Trailer', 'Trailer do Jogo Stray', 'https://www.youtube.com/watch?v=b-ugdyfd0ao', 2);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('TOUR PELO MOTORHOME CAMINHAO 4X4', 'Vlog sobre Motorhome', 'https://www.youtube.com/watch?v=3cqbCO2l1Bk', 3);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O MELHOR DO JAPAO', 'Vlog no Japao', 'https://www.youtube.com/watch?v=NZEAZIg4wRw', 3);
