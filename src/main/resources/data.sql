61088c2760e6894442954524aa5cdd4ea08dd876
Daniel Fonseca <daniel_mf@live.com>
2023-01-13 08:15:50 -0300

    Adição de Dados para Testes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,12 @@
+INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Livre', '#0000FF');
 INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Estudo', '#0000FF');
 INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Jogos', '#00FF00');
-INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Viagem', '#FF0000');
+INSERT INTO TB_CATEGORIAS(titulo, cor) VALUES('Viagem', '#FFFF00');
 
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Spring Boot | Curso Completo 2022', 'Aula sobre o Framework Spring Boot', 'https://www.youtube.com/watch?v=LXRU-Z36GEU', 1);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O que e Angular e AngularJS?', 'Aula sobre o Framework Angular', 'https://www.youtube.com/watch?v=LFlNU30u7d8', 1);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Os Jogos Mais Esperados de 2023', 'Lista de Jogos Aguardados em 2023', 'https://www.youtube.com/watch?v=S3ElEo41sCU', 2);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Stray - Trailer', 'Trailer do Jogo Stray', 'https://www.youtube.com/watch?v=b-ugdyfd0ao', 2);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Os 10 JOGOS mais Jogados Antigamente', 'Lista de Jogos mais jogados Antigamente', 'https://www.youtube.com/watch?v=Wpi_iFNvTHs', 2);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('TOUR PELO MOTORHOME CAMINHAO 4X4', 'Vlog sobre Motorhome', 'https://www.youtube.com/watch?v=3cqbCO2l1Bk', 3);
-INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O MELHOR DO JAPAO', 'Vlog no Japao', 'https://www.youtube.com/watch?v=NZEAZIg4wRw', 3);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Spring Boot | Curso Completo 2022', 'Aula sobre o Framework Spring Boot', 'https://www.youtube.com/watch?v=LXRU-Z36GEU', 2);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O que e Angular e AngularJS?', 'Aula sobre o Framework Angular', 'https://www.youtube.com/watch?v=LFlNU30u7d8', 2);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Os Jogos Mais Esperados de 2023', 'Lista de Jogos Aguardados em 2023', 'https://www.youtube.com/watch?v=S3ElEo41sCU', 3);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Stray - Trailer', 'Trailer do Jogo Stray', 'https://www.youtube.com/watch?v=b-ugdyfd0ao', 3);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('Os 10 JOGOS mais Jogados Antigamente', 'Lista de Jogos mais jogados Antigamente', 'https://www.youtube.com/watch?v=Wpi_iFNvTHs', 3);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('TOUR PELO MOTORHOME CAMINHAO 4X4', 'Vlog sobre Motorhome', 'https://www.youtube.com/watch?v=3cqbCO2l1Bk', 4);
+INSERT INTO TB_VIDEOS(titulo, descricao, url, categoria_id) VALUES('O MELHOR DO JAPAO', 'Vlog no Japao', 'https://www.youtube.com/watch?v=NZEAZIg4wRw', 4);
